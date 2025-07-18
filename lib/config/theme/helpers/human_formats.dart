29402357092ee942eae322b410201e4ac063e278
manuguzmanx <luisvg18@gmail.com>
2025-07-18 16:01:43 -0600

    Proyecto finalizado

---
+++ lib/config/theme/helpers/human_formats.dart
@@ -0,0 +1,15 @@
+import 'package:intl/intl.dart';
+
+class HumanFormats {
+
+  static String humanReadableNumber(double number) {
+
+    final formattedNumber = NumberFormat.compactCurrency(
+      decimalDigits: 0,
+      symbol: ''
+    ).format(number);
+    
+    return formattedNumber;
+
+  }
+}
